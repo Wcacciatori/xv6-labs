8f0f8c5b46d7752a8bb11c12732c1995c63f915b
wjy <2786484775@qq.com>
2024-07-29 14:52:29 +0800

    lab1 primes 添加详细注释

--- user/primes.c
+++ user/primes.c
@@ -6,42 +6,39 @@
 void prime(int lpipe[2]);
 int main(int argc, char const *argv[])
 {
-    // close(0);
-    // close(1);   
     int i = 0;
     int p[2];
-    int *buf;
+
 
     pipe(p);
 
-    for(i=2; i<36; i++){
-        buf = &i;
-        write(p[1], buf, sizeof(i));
+    for(i=2; i<36; i++){//把所有数据送进管道
+        write(p[1], &i, sizeof(i));
     }
 
-    prime(p);
+    prime(p);//开始试炼
 
     exit(0);
 }
 
 void prime(int lpipe[2]){
-    close(lpipe[1]);
-    int first, num;
-        if(read(lpipe[0], &first, sizeof(first)) != 0){
-            printf("prime %d\n", first);
+    close(lpipe[1]);//先把左邻居的写端关了
+    int first, num;//用来保存从管道中读出的数据，第一个（打印）和其余的继续判断
+        if(read(lpipe[0], &first, sizeof(first)) != 0){//把第一个读出来
+            printf("prime %d\n", first);//打印
             if(fork() == 0){//在子进程中进行发送工作
-                int rpipe[2];
-                pipe(rpipe);
-                while(read(lpipe[0], &num, sizeof(num))){
-                    if((num % first)!=0){
-                        write(rpipe[1], &num, sizeof(num));
+                int rpipe[2];//定义右管道的两个口
+                pipe(rpipe);//创建管道
+                while(read(lpipe[0], &num, sizeof(num))){//循环从管道中读数据
+                    if((num % first)!=0){//不能被第一个数整除，说明可能是素数（若能被整除则一定不是素数）
+                        write(rpipe[1], &num, sizeof(num));//把有成为素数潜力的同志们送到下一次试炼
                     }
                 }
-                close(lpipe[0]);
-                prime(rpipe);
+                close(lpipe[0]);//关闭子进程中的左邻居读端
+                prime(rpipe);//开启下一次试炼
             }else{
-                close(lpipe[0]);
-                wait(0);
+                close(lpipe[0]);//关闭父进程中的左邻居读端
+                wait(0);//父进程等子进程结束，否则同时进行打印要出错了
             }
         }
 
